0084cd2e663e11acd18faa89d67f22b3ab71acfc
slavco zute <slav.zute@jdplc.com>
2018-07-21 13:39:27 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+      app: './src/index.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Hot Module Replacement',
+      template: './index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader"
+        ]
+    },
+    {
+      test: /\.(png|jpg|gif)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {}
+        }
+      ]
+    },
+    {
+      test: /\.(html)$/,
+      use: {
+        loader: 'html-loader',
+        // options: {
+        //   attrs: [':data-src']
+        // }
+      }
+    }]
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
