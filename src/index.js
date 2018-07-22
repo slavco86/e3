29bc82dd9da36c1323b09daab4339b2474e43671
slavco zute <slav.zute@jdplc.com>
2018-07-22 21:08:22 +0100

    implement carousel and finish styling

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import './../index.html';
 import './styles/styles.scss';
+import Swiper from 'swiper';
+
+var carousel = new Swiper('.swiper-container', {
+  slidesPerView: 3.5,
+  centeredSlides: true,
+  spaceBetween: 50,
+  loop: true,
+  breakpoints: {
+    765: {
+      slidesPerView: 1.5,
+      spaceBetween: 25,
+    }
+  },
+  navigation: {
+    nextEl: '.nav-next',
+    prevEl: '.nav-prev',
+  },
+  grabCursor: true,
+});
